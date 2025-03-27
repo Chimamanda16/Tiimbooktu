c9ce36f30255ea51233a8e9ce285a39edfc45870
Chimamanda16 <chimamandaunachukwu@gmail.com>
2025-03-27 11:10:40 +0100

    Add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import HomeComp from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ function HeroComp(){
     return(
         <div className="hero-cont">
             <h1 className="hero-text">We're la culture class Re Birth thee Re-Renaissance</h1>
-            <img className="hero-img" src="../public/hero-img.png" alt="" />
+            <img className="hero-img" src="/hero-img.png" alt="" />
         </div>
     )
 }
