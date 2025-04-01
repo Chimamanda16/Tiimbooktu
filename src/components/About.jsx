585ddb0523c9434ce656e8e0e0b3e8563585735f
Chimamanda16 <chimamandaunachukwu@gmail.com>
2025-04-01 14:21:23 +0100

    Convert footer section to tailwind

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,12 @@
 function AboutComp() {
     return (
         <div className="mb-[10%]">
-            <h2 className="text-[112px] font-[MyFont] ml-[15%] md:text-3xl">ABOUT TIIMBOOKTU</h2>
-            <div className="w-[50%] ml-[30%] md:w-fit md:mr-[5%]">
+            <h2 className="text-[112px] font-[MyFont] ml-[15%] max-md:text-3xl mb-[2%]">ABOUT TIIMBOOKTU</h2>
+            <div className="w-[50%] ml-[30%] max-md:w-fit md:mr-[5%]">
                 <p>
                     Lorem ipsum dolor sit amet consectetur. Vulputate commodo sit massa vitae. Sagittis tempor tempus ac sodales elementum eu convallis dui malesuada. Neque vulputate non nunc in gravida fringilla arcu id quis. Diam massa purus dictum sed. Vitae mi suspendisse a est cras. Sit varius egestas laoreet metus facilisi. Magna quisque tellus magna felis vitae.
                 </p>
-                <button className="bg-[#CDFFAD] border-none rounded-[22px] text-[#1c1c1c] cursor-pointer font-normal px-4 py-2">
+                <button className="bg-[#CDFFAD] border-none rounded-[22px] text-[#1c1c1c] cursor-pointer font-normal px-4 py-2 mt-[2%]">
                     Learn More
                 </button>
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,25 @@
-import "../Styles/Footer.css";
-
-function FooterComp(){
-    return(
-        <div className="footer">
-            <div className="footer-cont">
-                <div className="subscribe">
-                    <p className="about-sub">By The Way</p>
-                    <h2>SUBSCRIBE TO OUR NEWSLETTERS</h2>
-                    <input type="email" placeholder="Email"/>
+function FooterComp() {
+    return (
+        <div className="p-[2%_10%] md:p-[2%_5%]">
+            <div className="flex justify-between">
+                {/* Subscribe Section */}
+                <div className="w-[70%] sm:w-full lg:w-[70%]">
+                    <p className="text-[#808080] text-[10px] mb-[5%]">By The Way</p>
+                    <h2 className="text-[40px] font-medium font-[MyFont] max-md:text-[30px]">
+                        SUBSCRIBE TO OUR NEWSLETTERS
+                    </h2>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-[70%] max-md:w-[80%] bg-transparent border border-[#34343C] max-md:p-[5%] md:p-[3%]"
+                    />
                 </div>
-                <div className="about-footer">
-                    <p className="about-sub">About Us</p>
-                    <div className="footer-links">
-                        <div>
+
+                {/* About & Links Section */}
+                <div className="w-[30%] sm:w-full lg:w-[30%]">
+                    <p className="text-[#808080] text-[10px] mb-[2%]">About Us</p>
+                    <div className="flex justify-between">
+                        <div className="md:mr-[10%]">
                             <p>Tiimbooktu</p>
                             <p>Thought</p>
                             <p>Fotografie</p>
@@ -24,21 +31,20 @@ function FooterComp(){
                         </div>
                     </div>
 
+                    {/* Contact & Privacy Section */}
                     <div>
-                        <p className="about-sub">Contact Us</p>
+                        <p className="text-[#808080] text-[10px] mb-[2%] mt-[4%]">Contact Us</p>
                         <div>
                             <p>+1 (001) 981-76-17</p>
                             <p>info@tiimbooktu</p>
                         </div>
-                        <p className="about-sub">Privacy</p>
-                        <p className="about-sub">2025-Copyright</p>
+                        <p className="text-[#808080] text-[10px] mt-[4%]">Privacy</p>
+                        <p className="text-[#808080] text-[10px]">2025-Copyright</p>
                     </div>
                 </div>
             </div>
-            
-
         </div>
-    )
+    );
 }
 
 export default FooterComp;
