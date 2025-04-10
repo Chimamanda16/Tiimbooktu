94950675678b635f467a0f05bb931befc229ed95
Chimamanda16 <chimamandaunachukwu@gmail.com>
2025-04-10 13:58:47 +0100

    Update Hero and About Sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 function HeroComp() {
     return (
-        <div className="flex p-10 mb-[10%]">
-            <h1 className="font-[chango] lg:text-[50px] uppercase xl:text-[50px] w-[90%] max-w-[1240px] mx-auto my-[20px] break-words max-lg:text-5xl max-lg:max-w-[70%] max-md:text-4xl max-md:max-w-[80%]">
+        <div className="flex md:px-[100px] max-lg:block py-[60px] mb-[10%]">
+            <h1 className="font-[chango] lg:text-[50px] uppercase max-md:text-[30px] my-[20px] break-words max-lg:text-5xl max-md:text-4xl">
             We're<br /><span className="text-[#CDFFAD]"> la cultura.</span><br /> class.<br /> Re Birth.<br /> Re-Renaissance
             </h1>
             <img className="h-[534px] w-full object-cover" src="/hero-img.png" alt="" />
