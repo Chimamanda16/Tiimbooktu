0002df139b0a48018c20c0d3d78f1190cdc6a1a2
Chimamanda16 <chimamandaunachukwu@gmail.com>
2025-04-15 11:44:23 +0100

    Add Signup Page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useAuthStore } from "../Store/useAuthStore.js"
+import NavBarComp from "../components/Navbar";
+import FooterComp from "../components/Footer";
+
 const SignUpPage = () => {
 
     const { register } = useAuthStore();
@@ -25,31 +28,37 @@ const SignUpPage = () => {
     }
 
   return (
-    <div className="text-[#000] text-center min-h-screen py-6">
-        <h2 className="mb-4 font-bold">Register</h2>
-        <form className="w-[80%] m-auto text-[#000]" onSubmit={submitForm}>
-            <div>
-                <input className="border-2 placeholder:text-[#000] border-[#000] mb-6 px-10 py-2 w-[40%]" type="text" id="username" name="name" placeholder="Name" value={formData.name} required onChange={(e) =>(setFormData((formData) =>({ ...formData, name: e.target.value })))} autoComplete="name"/>
+    <div className="bg-[#1c1c1c] w-full text-white">
+        <NavBarComp />
+        <div className="md:w-[510px] m-auto my-10 text-[#fff] text-center py-6">
+        <h2 className="mb-4 font-[400] font-chango text-[26px]">Sign Up</h2>
+        <form className="text-left m-auto mt-6 my-10 bg-[#0A0A0A] w-[90%] p-[15px] rounded-[20px]" onSubmit={submitForm}>
+            <div className="flex gap-2">
+                <div>
+                    <label className="block" htmlFor="first name">First Name</label>
+                    <input className="border-2 placeholder:text-[#D9D9D9] border-[#000] mb-6 px-10 py-2" type="text" id="username" name="first name" placeholder="Name" value={formData.name} required onChange={(e) =>(setFormData((formData) =>({ ...formData, name: e.target.value })))} autoComplete="name"/>
+                </div>
+                <div>
+                    <label className="block" htmlFor="name">Last Name</label>
+                    <input className="border-2 text-[#D9D9D9] border-[#000] mb-6 px-10 py-2" type="text" id="username" name="name" placeholder="Name" value={formData.name} required onChange={(e) =>(setFormData((formData) =>({ ...formData, name: e.target.value })))} autoComplete="name"/>
+                </div>
             </div>
             <div>
-                <input className="border-2 placeholder:text-[#000] border-[#000] mb-6 px-10 py-2 w-[40%]" type="email" id="email" name="email" placeholder="Email" value={formData.email} required onChange={(e) =>(setFormData({...formData, email: e.target.value}))} autoComplete="email"/>
+                <input className="border-2 placeholder:text-[#D9D9D9] border-[#000] mb-6 px-10 py-2" type="email" id="email" name="email" placeholder="Email" value={formData.email} required onChange={(e) =>(setFormData({...formData, email: e.target.value}))} autoComplete="email"/>
             </div>
             <div>
-                <select className="border-2 placeholder:text-[#000] border-[#000] mb-6 px-10 py-2 w-[40%]" name="type" placeholder="Type" id="type" value={formData.type} required onChange={(e) => (setFormData({ ...formData, type: e.target.value }))}>
-                    <option value="" disabled>Select a role</option>
-                    <option value="customer">customer</option>
-                    <option value="admin">admin</option>
-                </select>
+                <input className="border-2 placeholder:text-[#D9D9D9] border-[#000] mb-6 px-10 py-2" type="password" id="password" name="password" placeholder="Password" value={formData.password} required onChange={(e) =>(setFormData({...formData, password: e.target.value}))}/>
             </div>
             <div>
-                <input className="border-2 placeholder:text-[#000] border-[#000] mb-6 px-10 py-2 w-[40%]" type="password" id="password" name="password" placeholder="Password" value={formData.password} required onChange={(e) =>(setFormData({...formData, password: e.target.value}))}/>
+                <input className="border-2 placeholder:text-[#D9D9D9] border-[#000] mb-6 px-10 py-2" type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" value={formData.password_confirmation} required onChange={(e) =>(setFormData({...formData, password_confirmation: e.target.value}))}/>
             </div>
-            <div>
-                <input className="border-2 placeholder:text-[#000] border-[#000] mb-6 px-10 py-2 w-[40%]" type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm Password" value={formData.password_confirmation} required onChange={(e) =>(setFormData({...formData, password_confirmation: e.target.value}))}/>
-            </div>
-            <button className="bg-[#000] text-[#fff] px-6 py-2 mt-4" type="submit">Register</button>
+            <button className="bg-[#CDFFAD] w-full rounded-[12px] text-black px-6 py-2 my-4" type="submit">Send</button>
+            <p className="text-[#D9D9D9] text-center">Already Have An Account? <span className="underline font-bold">Signin</span></p>
+
         </form>
     </div>
+    <FooterComp />
+    </div>  
   )
 }
 
