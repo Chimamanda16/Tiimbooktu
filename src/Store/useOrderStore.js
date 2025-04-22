ed30bd6c465d422affa7932ae7bce2d76e76d31a
Fatomi02 <fatomiabdulrahmon@gmail.com>
2025-04-22 05:06:57 -0700

    order page

--- src/Store/useOrderStore.js
+++ src/Store/useOrderStore.js
@@ -1,18 +1,33 @@
 import axiosInstance from "../lib/axios";
 import { create } from "zustand";
-import { toast } from "react-toastify";
 
-export const useOrderStore = create((set, get) => ({
+export const useOrderStore = create((set) => ({
     fetchingOrders: false,
     orders: {
         data: []
     },
+    orderDetail: null,
     error: null,
     fetchOrders: async() => {
         set({fetchingOrders: true})
         try {
             const res = await axiosInstance.get('/orders');
             set({ orders: res?.data })
+            return res.data
+        } 
+        catch (err) {
+            console.error('Error fetching artworks:', err);
+            set({error: err.message})
+        } 
+        finally {
+            set({fetchingOrders: false})
+        }
+    },
+    fetchOrder: async(id) => {
+        try {
+            const res = await axiosInstance.get(`/orders/${id}`);
+            console.log(res)
+            set({ orderDetail: res?.data.data })
             return res.data
         } 
         catch (err) {
