df73bee7a586cf14efb101053d067bbf67e69d21
chizu praise <chizupraise31@gmail.com>
2025-04-24 23:22:00 +0100

    feat:redesigned home page and it's component, redesigned footer component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,43 @@
 import { Link } from "react-router-dom";
 
 function AboutComp() {
-    return (
-        <div className="mb-[10%] w-[90%] mx-auto font-cinzel flex flex-col gap-12">
-            <h2 className="lg:text-[80px] font-chango uppercase text-[26px] mb-[2%]">Feat. Post</h2>
-            <div className="grid lg:grid-cols-2 gap-8">
-            <h4 className="font-chango text-[22px] lg:hidden uppercase">I am my hair</h4>
+  return (
+    <div className="bg-[#0A0A0A]">
+      <div className="mb-[10%] py-20 w-[90%] mx-auto font-cinzel flex flex-col gap-12 ">
+        <h2 className="lg:text-[80px] font-chango uppercase text-[26px] mb-[2%]">
+          Feat. Post
+        </h2>
+        <div className="grid lg:grid-cols-2 gap-8 pb-10">
+          <h4 className="font-chango text-[22px] lg:hidden uppercase">
+            I am my hair
+          </h4>
 
-                <img className="w-full max-h-[500px]" src="/assets/images/home-hair.png" alt="" />
-                <div className="w-full flex flex-col items-start justify-end gap-6">
-                    <h4 className="font-chango hidden lg:block uppercase">I am my hair</h4>
-                    <p className="lg:text-[22px]">
-                    "... it's toughness and strength. I am a city wall built against attacks; built to build from ashes and adversity: built to use storms and rain and storm to toughen me up to the point of yelling: "IS THERE NO ONE ELSE?"
-                    </p>
-                    <Link to='/my-hair' className="bg-[#CDFFAD] block border-none rounded-[22px] text-[#1c1c1c] cursor-pointer font-normal px-4 py-2">
-                        Learn More
-                    </Link>
-                </div>
-            </div>
+          <img
+            className="w-full max-h-[500px]"
+            src="/assets/images/home-hair.png"
+            alt=""
+          />
+          <div className="w-full flex flex-col items-start justify-end gap-6">
+            <h4 className="font-chango hidden lg:block uppercase">
+              I am my hair
+            </h4>
+            <p className="lg:text-[22px]">
+              "... it's toughness and strength. I am a city wall built against
+              attacks; built to build from ashes and adversity: built to use
+              storms and rain and storm to toughen me up to the point of
+              yelling: "IS THERE NO ONE ELSE?"
+            </p>
+            <Link
+              to="/my-hair"
+              className="bg-[#CDFFAD] block border-none rounded-[22px] text-[#1c1c1c] cursor-pointer font-normal px-4 py-2"
+            >
+              Learn More
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default AboutComp;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,14 +1,14 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { useCartStore } from '../Store/useCartStore';
-import { useEffect } from 'react';
-import { useArtworkStore } from '../Store/useArtworkStore';
-import { useWishlistStore } from '../Store/useWishlistStore';
+import { Link, useNavigate } from "react-router-dom";
+import { useCartStore } from "../Store/useCartStore";
+import { useEffect } from "react";
+import { useArtworkStore } from "../Store/useArtworkStore";
+import { useWishlistStore } from "../Store/useWishlistStore";
 function ShopComp() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { artworks, fetchingArtwork, fetchArtworks } = useArtworkStore();
-  const {fetchWishlist, addToWishlist, removeFromWishlist, wishlistItems} = useWishlistStore();
+  const { fetchWishlist, addToWishlist, removeFromWishlist, wishlistItems } =
+    useWishlistStore();
   const { addToCart } = useCartStore();
-
 
   useEffect(() => {
     fetchArtworks();
@@ -17,10 +17,9 @@ function ShopComp() {
   function addToFavorites(e, id) {
     e.stopPropagation();
 
-    if(wishlistItems?.some((item) => item.id === id)){
+    if (wishlistItems?.some((item) => item.id === id)) {
       removeFromWishlist(id);
-    }
-    else{
+    } else {
       addToWishlist(id);
     }
   }
@@ -32,52 +31,88 @@ function ShopComp() {
     };
     e.stopPropagation();
     addToCart(payload);
-  }
+  };
 
   const goToDetail = (e, id) => {
     e.stopPropagation();
-    navigate(`/detail/${id}`)
-  }
+    navigate(`/detail/${id}`);
+  };
 
   return (
-    <div className="w-[90%] mx-auto text-center flex flex-col gap-[30px] items-center font-cinzel pb-[50px] lg:pb-[100px] text-white">
+    <div className="w-[90%] mx-auto text-center flex flex-col gap-[30px] items-center font-cinzel py-20  text-white">
       <h2 className="font-chango text-[26px] md:text-[40px] lg:text-[50px] tracking-[2px]">
         THE SHOP
       </h2>
 
       <div className="grid w-full grid-cols-2 justify-center gap-4 md:grid-cols-3 lg:grid-cols-4 mb-8 lg:mb-16">
-        {fetchingArtwork ? (<h2 className='text-white text-start w-full'>Loading...</h2>) :
-          (artworks?.length > 0) ? artworks?.map((artwork) => (
-            <div onClick={(e) => goToDetail(e, artwork.id)} key={artwork.id} className="flex flex-col cursor-pointer justify-between items-center border border-[#353535] lg:p-4 gap-2 pb-2">
+        {fetchingArtwork ? (
+          <h2 className="text-white text-start w-full">Loading...</h2>
+        ) : artworks?.length > 0 ? (
+          artworks?.map((artwork) => (
+            <div
+              onClick={(e) => goToDetail(e, artwork.id)}
+              key={artwork.id}
+              className="flex flex-col cursor-pointer justify-between items-center border border-[#353535] lg:p-4 gap-2 pb-2"
+            >
               <div className="relative w-[90%] h-[251px]">
-                <img src={artwork.images[0].url} alt="" className="w-full h-full object-cover block"/>
+                <img
+                  src={artwork.images[0].url}
+                  alt=""
+                  className="w-full h-full object-cover block"
+                />
                 <div className="absolute top-2 right-2 bg-[#595959] p-2 rounded-full">
                   <img
-                  src={wishlistItems?.some((item) => item.id === artwork.id)
-                    ? "./assets/icons/favorite-heart-black.svg"
-                    : "./assets/icons/favorite-heart-white.svg"}
-                  alt=""
-                  className="w-[24px] h-[24px]"
-                  onClick={(e) => addToFavorites(e, artwork.id)} />                
+                    src={
+                      wishlistItems?.some((item) => item.id === artwork.id)
+                        ? "./assets/icons/favorite-heart-black.svg"
+                        : "./assets/icons/favorite-heart-white.svg"
+                    }
+                    alt=""
+                    className="w-[24px] h-[24px]"
+                    onClick={(e) => addToFavorites(e, artwork.id)}
+                  />
                 </div>
               </div>
               <p className="capitalize">{artwork.name}</p>
               <p>${artwork.base_price}</p>
-              <button onClick={(e) => addArtToCart(e, artwork)}
-                className="bg-transparent border border-[#a9a9a9] text-white font-bold py-2 px-4 cursor-pointer max-sm:py-1 max-sm:text-xs">
+              <button
+                onClick={(e) => addArtToCart(e, artwork)}
+                className="bg-transparent border border-[#a9a9a9] text-white font-bold py-2 px-4 cursor-pointer max-sm:py-1 max-sm:text-xs"
+              >
                 Add To Cart
               </button>
             </div>
-          )) : 'No Artwork Found'
-        }
-
+          ))
+        ) : (
+          "No Artwork Found"
+        )}
       </div>
 
-      <Link to="/shop" className="bg-[#cdffad] text-[#1c1c1c] font-normal rounded-[22px] px-4 py-2 cursor-pointer text-lg max-sm:text-base max-sm:px-5 max-sm:py-3">
+      <Link
+        to="/shop"
+        className="bg-[#cdffad] text-[#1c1c1c] font-normal rounded-[22px] px-4 py-2 cursor-pointer text-lg max-sm:text-base max-sm:px-5 max-sm:py-3"
+      >
         View All
       </Link>
+      <div className="w-[90%] max-w-6xl mx-auto my-20">
+        <div className="text-center mb-8">
+          <h2 className="text-3xl md:text-7xl font-bold mb-12 font-[Chango]">
+            SIGN UP FOR NEWSLETTERS
+          </h2>
+        </div>
+        <div className="flex flex-col md:flex-row gap-4 justify-center max-w-3xl mx-auto">
+          <input
+            type="email"
+            placeholder="Enter Your E-Mail"
+            className="flex-grow py-3 px-4 bg-transparent border border-gray-700 text-white"
+          />
+          <button className="bg-green-200 text-black py-3 px-8 font-medium">
+            Subscribe
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default ShopComp;  
+export default ShopComp;
